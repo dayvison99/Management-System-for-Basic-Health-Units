6d8bb2b13a075809f913d430ab38ecad845e4ab6
dayvison99 <dayvison.marley@gmail.com>
2016-12-19 08:47:02 -0200

    Criação da class Medicamento

---
+++ Sistema-SGUBS/SGUBS/src/br/edu/ifnmg/sgu/Aplicacao/Medicamento.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.sgu.Aplicacao;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author dayvison
+ */
+public class Medicamento {
+    private int id;
+    private String descricao;
+    private int quantidade;
+    private String nome;
+
+    public Medicamento() {
+    }
+
+    public Medicamento(int id, String descricao, int quantidade, String nome) {
+        this.id = id;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + this.id;
+        hash = 97 * hash + Objects.hashCode(this.descricao);
+        hash = 97 * hash + this.quantidade;
+        hash = 97 * hash + Objects.hashCode(this.nome);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Medicamento other = (Medicamento) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.quantidade != other.quantidade) {
+            return false;
+        }
+        if (!Objects.equals(this.descricao, other.descricao)) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
